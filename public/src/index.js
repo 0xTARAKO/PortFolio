9e3b1a265d12bcee97cff85796f78c5bd8f7bbf7
0xtarako <bunker0xtarako@gamil.com>
2023-02-15 16:21:45 +0900

    update

--- public/src/index.js
+++ public/src/index.js
@@ -3,7 +3,6 @@ import { Crypto } from './crypto.js'
 
 const canvas = new Canvas()
 const crypto = new Crypto( canvas.ctx )
-const chat = new Chat()
 
 onresize = () => canvas.Resize()
 onkeydown = onkeyup = onmousemove = event => canvas.Control( event )
@@ -11,6 +10,4 @@ onclick = event => {
     if( event.target.id === 'volume' ) crypto.Sort( true )
     if( event.target.id === 'percent' ) crypto.Sort( false )
     if( event.target.id === 'crypto' )  canvas.tps.position.set( 0 , 2 , -5 ) , crypto.Draw( event.target.data )
-    if( event.target.id === 'send' ) chat.Send()
-    if( event.target.id === 'open' ) chat.Look()
 }
